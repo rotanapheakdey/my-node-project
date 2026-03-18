736b260ba592e993f35be8a251c180e89e21dece
rotanapheakdey <pheakdeyrotana1@gmail.com>
2026-03-18 14:25:24 +0700

    fix the eslint version problem

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,37 @@
-import js from "@eslint/js";
-import jest from "eslint-plugin-jest";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import jest from 'eslint-plugin-jest';
+import globals from 'globals';
+// import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["tests/**/*.test.js"], plugins: { jest }, rules: { ...jest.configs.recommended.rules }, languageOptions: { globals: globals.jest } },
-]);
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+  },
+
+  {
+    files: ['tests/**/*.test.js'],
+    plugins: { jest },
+    rules: {
+      ...jest.configs.recommended.rules,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+];
